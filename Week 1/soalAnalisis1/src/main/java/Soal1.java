69dc09142cbf51190172d6aa589c6882166cc75d
najwanzaky <117468891+najwanzakyahmad@users.noreply.github.com>
2023-08-24 19:21:03 +0700

    [Add] folder hasil praktek minggu pertama
    berisi folder welcome, soal analisis 1, dan soal analisis 2. folder dibuat menggunakan netbeans

---
+++ Week 1/soalAnalisis1/src/main/java/Soal1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class Soal1 {
+
+    public static void main(String[] args) {
+        byte angka1 = 125;
+        byte angka2 = 6;
+        byte hasil = (byte) (angka1 + angka2);
+
+        System.out.println("Hasil 1 " + hasil);
+    }
+}
