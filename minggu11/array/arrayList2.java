a3bf327d2a9340834f582b3237448c570f31300a
najwanzaky <117468891+najwanzakyahmad@users.noreply.github.com>
2023-11-10 23:36:44 +0700

    menambahkan folder minggu11
    minggu 11 belajar tentang list : waktu penggunaan yang tepat, perbedaan dan cara penggunaan

---
+++ minggu11/array/arrayList2.java
@@ -0,0 +1,19 @@
+package minggu11.array;
+
+import java.util.*;
+
+public class arrayList2 {
+  public static void main(String[] args) {
+    // Creae a list and add some colors to the list
+    List<String> list_Strings = new ArrayList<String>();
+    list_Strings.add("Red");
+    list_Strings.add("Green");
+    list_Strings.add("Orange");
+    list_Strings.add("White");
+    list_Strings.add("Black");
+    // Print the list
+    for (String element : list_Strings) {
+      System.out.println(element);
+    }
+  }
+}
---
+++ minggu11/treeSet/treeSet2.java
@@ -0,0 +1,17 @@
+package minggu11.treeSet;
+
+import java.util.TreeSet;
+public class treeSet2 {
+  public static void main(String[] args) {
+  TreeSet<String> tree_set = new TreeSet<String>();
+  tree_set.add("Red");
+  tree_set.add("Green");
+  tree_set.add("Orange");
+  tree_set.add("White");
+  tree_set.add("Black");
+  // Print the tree list
+  for (String element : tree_set) {
+    System.out.println(element);
+    }
+ }
+}
